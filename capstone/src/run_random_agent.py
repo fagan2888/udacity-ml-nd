0bb92571b690da54796693e484e5e0898619eed9
Dennis Francis <dennisfrancis.in@gmail.com>
2017-01-03 20:29:15 +0530

    Save mean and std of state vectors in pickle file

--- capstone/src/run_random_agent.py
+++ capstone/src/run_random_agent.py
@@ -1,6 +1,8 @@
 
 from agents import RandomAgent
 from experiments import Experiment
+import matplotlib.pyplot as plt
+import pickle
 
 import gym
 
@@ -16,8 +18,29 @@ exp.run()
 
 mean, std = ragent.describe_state_variables()
 
+fig = plt.figure()
+plt.subplot(211)
+plt.bar(list(xrange(0,8)), mean)
+plt.xlabel('State space components')
+plt.ylabel('Mean')
+plt.title('Mean of state space components')
+
+plt.subplot(212)
+plt.bar(list(xrange(0,8)), std)
+plt.xlabel('State space components')
+plt.ylabel('Standard deviation')
+plt.title('Standard deviation of state space components')
+
+fig.tight_layout()
+
 print 'Mean of observed states ='
 print mean
 
 print 'Standard deviation of observed states ='
 print std
+
+pickle.dump( (mean, std), open( "../params/state-stats.pkl", "wb" ) )
+
+fig.savefig('../log/state-space.png')
+
+plt.show()
