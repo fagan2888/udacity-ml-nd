f5cd9f5cd198bf85a7d982753977c1165209d735
Dennis Francis <dennisfrancis.in@gmail.com>
2017-01-11 19:33:45 +0530

    improved genplots script by adding mean and standard dev measure to the plots

--- capstone/src/genplots.py
+++ capstone/src/genplots.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 infiles = ['../log/highbenchmark_test_data.csv',
-           '../monitor/LunarLander_RandomAgent-1_data.csv',
+           '../log/LunarLander_RandomAgent-1_train_data.csv',
            '../log/LunarLander_BasicDQNAgent-1_test_data.csv',
            '../log/LunarLander_FullDQNAgent-exper_test_data.csv']
 
 names = ['High Benchmark', 'Low Benchmark', 'Basic DQN', 'Improved DQN']
+names_pd_cols = ['a. High Benchmark', 'b. Low Benchmark', 'c. Basic DQN', 'd. Improved DQN']
 
 evalplotfiles = ['../report_src/img/highbenchmark_test_evaluation.png',
                  '../report_src/img/randomagent.png',
@@ -19,28 +20,44 @@ ffviz_en_plotfile = '../report_src/img/ffviz_en.png'
 
 numagents = len(infiles)
 numtest_episodes = 500
-df_er = pd.DataFrame(columns=names, index=xrange(1,numtest_episodes+1))
-df_en = pd.DataFrame(columns=names, index=xrange(1,numtest_episodes+1))
+df_er = pd.DataFrame(columns=names_pd_cols, index=xrange(1,numtest_episodes+1))
+df_en = pd.DataFrame(columns=names_pd_cols, index=xrange(1,numtest_episodes+1))
 
+summaryfile = open('../report_src/data/summary.csv', 'w')
+summaryfile.write('Agent name,E[R],STD[R],E[N],M-score\n')
 
 for idx in xrange(numagents):
     print '-'*70
     print 'Agent : %s' % names[idx]
     print '-'*70
-    evaluate(infiles[idx], evalplotfiles[idx], names[idx])
+    er, stdr, en, mscore = evaluate(infiles[idx], evalplotfiles[idx], names[idx])
+    summaryfile.write('%s,%.5f,%.5f,%.5f,%.5f\n' % (names[idx], er, stdr, en, mscore))
     print '='*70
     df = pd.read_csv(infiles[idx])
-    df_er[names[idx]] = df['reward'].values
-    df_en[names[idx]] = df['steps'].values
+    df_er[names_pd_cols[idx]] = df['reward'].values
+    df_en[names_pd_cols[idx]] = df['steps'].values
 
-
+summaryfile.close()
+mean = df_er.mean()
+std  = df_er.std()
 axs1 = df_er.hist(bins=100, alpha=1, figsize=(10, 8), layout=(4, 1), sharex=True)
+for ii in xrange(4):
+    axs1[ii][0].axvline(mean[ii],           color='g', linestyle='dashed', linewidth=2)
+    axs1[ii][0].axvline(mean[ii] - std[ii], color='r', linestyle='dashed', linewidth=2)
+    axs1[ii][0].axvline(mean[ii] + std[ii], color='r', linestyle='dashed', linewidth=2)
 fig1 = axs1[0][0].get_figure()
 #[ax[0].set_ylabel('Frequency') for ax in axs1]
 fig1.suptitle('Histograms of episode rewards of each agent', fontsize=13)
 fig1.text(0.5, 0.04, 'Episode reward', ha='center')
 fig1.text(0.04, 0.5, 'Frequency', va='center', rotation='vertical')
+
+mean = df_en.mean()
+std  = df_en.std()
 axs2 = df_en.hist(bins=100, alpha=1, figsize=(10, 8), layout=(4, 1), sharex=True)
+for ii in xrange(4):
+    axs2[ii][0].axvline(mean[ii],           color='g', linestyle='dashed', linewidth=2)
+    axs2[ii][0].axvline(mean[ii] - std[ii], color='r', linestyle='dashed', linewidth=2)
+    axs2[ii][0].axvline(mean[ii] + std[ii], color='r', linestyle='dashed', linewidth=2)
 fig2 = axs2[0][0].get_figure()
 #[ax[0].set_ylabel('Frequency') for ax in axs2]
 fig2.suptitle('Histograms of episode lengths of each agent', fontsize=13)
