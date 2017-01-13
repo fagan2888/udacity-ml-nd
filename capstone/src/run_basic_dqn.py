574eb2bbd18a27384ce02b6162a3167b12b93662
Dennis Francis <dennisfrancis.in@gmail.com>
2017-01-13 13:02:52 +0530

    improved run_basic_dqn.py by adding more comments

--- capstone/src/run_basic_dqn.py
+++ capstone/src/run_basic_dqn.py
@@ -9,14 +9,18 @@ from keras.optimizers import Adam
 
 import gym
 
+# Seed to be used for initializing the environment and agent for
+# repeatability
 seed = 16
 
+# Create LunarLander-v2 gym environment
 env = gym.make('LunarLander-v2')
+# Set seed for PRN generator of numpy, random module and gym env.
 np.random.seed(seed)
 random.seed(seed)
 env.seed(seed)
 
-## lr = 0.00025
+## Instantiate a basic DQN agent
 ragent = DQNAgent(name='BasicDQNAgent-1', state_dim=env.observation_space.shape[0], action_dim=env.action_space.n, epsdecay=0.975,
                   buffersize=500000, samplesize=32, minsamples=1000, gamma=0.99,
                   nnparams = {  # Basic DQN setting
@@ -25,6 +29,7 @@ ragent = DQNAgent(name='BasicDQNAgent-1', state_dim=env.observation_space.shape[
                       'optimizer'      : Adam(lr=0.00025),
                       'target_network' : False })
 
+# Create an experiment with the LunarLander env and basic DQN agent for 500 train/test episodes
 exp    = Experiment(env, ragent, logdir="../log", verbose=True, num_episodes=500)
 
 # Training trials
@@ -33,6 +38,7 @@ exp.run(testmode=False)
 # Test trials
 exp.run(testmode=True)
 
+# Evaluate the agent in test trials
 evaluate('../log/LunarLander_BasicDQNAgent-1_test_data.csv', '../log/LunarLander_BasicDQNAgent-1_test_evaluation.png', 'Basic DQN')
 
 #plt.show()
