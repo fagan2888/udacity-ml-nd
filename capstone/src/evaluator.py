a6b5619fa0c954c4a646388d43b6436eba1ec2f2
Dennis Francis <dennisfrancis.in@gmail.com>
2017-01-13 13:02:16 +0530

    improved evaluator.py by adding more comments

--- capstone/src/evaluator.py
+++ capstone/src/evaluator.py
@@ -2,13 +2,23 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Calculate key statistics of an agent from its data log and also
+# create a plot with reward and episode length panels with Mscore
+# printed above
 def evaluate(datafile, plotfname, agentname):
+    # Load data log for the agent
     df = pd.read_csv(datafile)
+    # Calculate expected reward
     er = df["reward"].mean()
+    # Calculate standard deviation of rewards
     stdr = df["reward"].std()
+    # Expected episode length
     en = df["steps"].mean()
+    # Scaling factor used in M-score formula
     psi = 0.00647582
+    # Sharpe ratio
     sharpe = er/stdr
+    # Print all stats to screen
     print 'E[R] = %.5f' % er
     print 'STD[R] = %.5f' % stdr
     print 'sharpe ratio = %.5f' % sharpe
@@ -16,11 +26,15 @@ def evaluate(datafile, plotfname, agentname):
     #print 'psi = %.8f' % (sharpe/en)
     score = 0.0
     if er > 0.0:
+        # M-score if expected reward is positive
         score = sharpe - psi*en
     else:
+        # M-score if expected reward is negative
         score = er
+    # Print M-score to screen
     print 'score = %.5f' % score
     ###
+    # create plots of rewards and episode lengths
     x = np.arange(1, df.shape[0]+1)
     fig = plt.figure(figsize=(12,9))
     plt1 = plt.subplot(211)
@@ -41,5 +55,7 @@ def evaluate(datafile, plotfname, agentname):
     plt.suptitle('%s agent score = %.4f' % (agentname, score), fontsize=15)
     plt.subplots_adjust(top=0.92)
     #print "psi = %.5f" % (1.0/score)
+    # Save the figure to speficied file name
     fig.savefig(plotfname)
+    # Return key statistics to caller
     return er, stdr, en, score
