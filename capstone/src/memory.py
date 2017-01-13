6c755770defaa54ab07c938f1706faeaaf2cc6ad
Dennis Francis <dennisfrancis.in@gmail.com>
2017-01-13 13:02:42 +0530

    improved memory.py by adding more comments

--- capstone/src/memory.py
+++ capstone/src/memory.py
@@ -3,6 +3,8 @@ import random
 class Memory(object):
     """ Memory buffer for storing past experiences of the agent """
     def __init__(self, maxsize=100000):
+        # Create lists for storing a set of observations
+        # Components of each observations are stored in separate lists
         self.maxsize = maxsize
         self.s  = []
         self.a  = []
@@ -12,11 +14,13 @@ class Memory(object):
 
     def save(self, s_, a_, r_, s1_, done_):
         """ Saves a single experience tuple to buffer """
+        # Add input observation to the component lists
         self.s.append(s_)
         self.a.append(a_)
         self.r.append(r_)
         self.s1.append(s1_)
         self.done.append(done_)
+        # If the buffer is overflowing, remove the oldest added observation
         if len(self.s) > self.maxsize:
             self.s.pop(0)
             self.a.pop(0)
@@ -30,9 +34,14 @@ class Memory(object):
 
     def sample(self, n):
         """ Returns n random experience tuples from the buffer """
+        # Current number of observations stored
         N    = len(self.s)
         n    = min(n, N)
+        # Sample a list of n random indices from the range [0, N)
+        # without replacement
         ids  = random.sample(xrange(N), n)
+        # Create slices of each observation component using
+        # this list of random indices
         s    = [self.s[ii] for ii in ids]
         a    = [self.a[ii] for ii in ids]
         r    = [self.r[ii] for ii in ids]
